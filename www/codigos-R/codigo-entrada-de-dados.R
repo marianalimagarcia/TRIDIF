39bce4acd1100cde3388f66b9aa273fd3903ea65
Mariana Lima Garcia <marianalimagarcia@gmail.com>
2021-12-04 12:10:14 -0300

    Delete www directory

--- www/codigos-R/codigo-entrada-de-dados.R
+++
@@ -1,8 +0,0 @@
-DF = read.csv(file   = "nomedoarquivo.csv")
-
-#se os dados não tiverem a coluna grupo
-matriz = DF
-
-#se os dados tiverem a coluna grupo
-matriz = DF[, 1:(ncol(DF) - 1)]
-grupo = DF[, ncol(DF)]
